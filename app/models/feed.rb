b710910014035011f2ec0232195201bc9a8eba1b
Zack Olson <zacharykennetholson@gmail.com>
2015-08-24 13:06:59 -0700

    embedded videos working with custom sanitization options hash

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -6,6 +6,19 @@ class Feed < ActiveRecord::Base
     fetch_failure: "We were unable to connect to your feed, make sure that you've entered the correct URL"
   }
 
+  SANITIZATION_OPTIONS = Sanitize::Config.merge(Sanitize::Config::RELAXED,
+                          :elements => Sanitize::Config::RELAXED[:elements] + ['video', 'source'],
+                          :attributes => Sanitize::Config.merge(Sanitize::Config::RELAXED[:attributes], 
+                           'video' => ['width', 'height', 'data-crt-options', 'poster'],
+                           'source' => ['src', 'type'] 
+                          ),
+
+                          :protocols => Sanitize::Config.merge(Sanitize::Config::RELAXED[:protocols],
+                            'a'   => {'href' => ['ftp', 'http', 'https', 'mailto']},
+                            :all => {'src'  => ['http', 'https', :relative]}
+                          )
+                        )
+
   validates :author, :url, presence: true
   validates_uniqueness_of :url, message: "This feed has already been added."
   validate :parsable
@@ -38,7 +51,7 @@ class Feed < ActiveRecord::Base
             ["timestamp", "posted #{time_ago_in_words(value)} ago."]
           else
             if %w[title author summary content image].include?(key)
-              value = Sanitize.fragment(value, Sanitize::Config::RELAXED)
+              value = Sanitize.fragment(value, SANITIZATION_OPTIONS)
             end
             [key, value] 
           end
@@ -78,21 +91,18 @@ class Feed < ActiveRecord::Base
   end
 
   def generate_topics
-    created_topics = Hash.new { |h, k| h[k] = 0 }
+    feed_topics = Hash.new { |h, k| h[k] = 0 }
     entries.each do |entry|
       return unless entry['categories']
-      JSON.parse(entry['categories']).each do |category|
-        
-
-        # topic = Topic.find_by_title(category.downcase)
-        # unless topic
-        #   topic = Topic.create(title: category.downcase)
-        # end
-        # unless created_topics[topic]
-        #   FeedTopic.create(feed_id: id, topic_id: topic.id)
-        #   created_topics[topic] = true
-        # end
+      JSON.parse(entry['categories']).each do |topic|
+        feed_topics[topic] += 1
       end
+    end
+
+    feed_topics.keys.sort_by {|k| -feed_topics[k] }[0..4].each do |hot_topic|
+      topic = Topic.find_by_title(hot_topic.downcase)
+      topic = Topic.create(title: hot_topic.downcase) unless topic
+      FeedTopic.create(feed_id: id, topic_id: topic.id)
     end
   end
 end
