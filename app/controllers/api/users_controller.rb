5c4845ac582811a4214db12294e35e12fad6e141
Zack Olson <zacharykennetholson@gmail.com>
2015-08-25 07:39:55 -0700

    add guest updating constraint

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,9 @@ class Api::UsersController < ApplicationController
 
   def update
     @user = current_user
-    if @user.update(user_params)
+    if @user.username == "Guest"
+      render :json => ["You're a guest!  Feel free to make an account and update away"], status: 422
+    elsif @user.update(user_params)
       render 'show'
     else
       render :json => @user.errors.full_messages, status: 422
